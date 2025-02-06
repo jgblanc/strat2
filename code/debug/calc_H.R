9db3d4f4490ab2aa0f776482fe1e64af6cb11465
jgblanc <jgblanc@uchicago.edu>
2025-02-06 12:08:46 -0600

    t

--- code/debug/calc_H.R
+++ code/debug/calc_H.R
@@ -15,13 +15,22 @@ H_outfile = args[3]
 
 # Read in FGR across blocks
 dfFGr <- fread(FGr_file)
+dfFGr <- as.data.frame(dfFGr[,2:ncol(dfFGr)])
+
+# Read in SNP file
+dfSNP <- fread(SNP_file)
+
+# Check variance of each block
+vars <- rep(0, 1703)
+for (i in 1:ncol(dfFGr)) {
+  L <- dfSNP$SNPcounter[i]
+  vars[i] <- var(dfFGr[,i] * (1/(sqrt(L-1))))
+}
+mean(vars)
 
 # Calculate FGr
 FGr_raw <- apply(dfFGr, 1, sum)
 print(paste0("The raw var is ", var(FGr_raw)))
-
-# Read in SNP file
-dfSNP <- fread(SNP_file)
 
 # Scale by 1/sqrt(L-1)
 L <- sum(dfSNP$SNPcounter)
@@ -30,10 +39,29 @@ FGr <- FGr_raw * (1/(sqrt(L-1)))
 print(paste0("The scaled var is ", var(FGr)))
 
 # Calculate H
-M <- nrow(df)
+M <- nrow(dfFGr)
 H <- (1/(M * (L-1))) * (t(FGr) %*% FGr)
 print(paste0("H is ", H))
 print(paste0("1/L is ", 1/L))
+
+# Compute SE for D
+nblocks <- ncol(dfFGr)
+print(nblocks)
+allHs <- rep(NA, nblocks)
+for (i in 1:nblocks) {
+
+  mi <- as.numeric(dfSNP[i, 2])
+  FGri <- dfFGr[,i] * (1/sqrt(mi-1))
+  Hi <- (sum(FGri^2)) * (1/M) * (1 / (mi -1))
+  allHs[i] <- (mi / (L - mi)) * (H - Hi)^2
+
+}
+varH <- mean(allHs)
+se <- sqrt(varH)
+
+pval <- pnorm(H ,mean =(1/L), sd = se, lower.tail = FALSE)
+print(pval)
+
 
 ## Save FGr
 #dfOut <- as.data.frame(cbind(df[,1], FGr))
