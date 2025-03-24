ca02407d378ce33d3c143f634445c4701cd47911
jgblanc <jgblanc@uchicago.edu>
2025-03-24 14:44:34 -0500

    t

---
+++ code/summarize/concat_varyParameters.R
@@ -0,0 +1,63 @@
+# This script concatenates all the overlap statistics from a given dataset
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Rscript concat_OverlapStats.R <output file> <input files>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+  library(tidyverse)
+}))
+
+
+outfile = args[1]
+
+dfOut <- matrix(NA, nrow = 1, ncol = 10)
+
+for (i in 2:length(args)) {
+
+  print(i)
+
+  # Get results file name
+  filename = args[i]
+
+  # Extract dataset
+  dataset <- strsplit(filename, "/")[[1]][3]
+
+  # Extract which GWAS
+  gwas <- strsplit(filename, "/")[[1]][4]
+
+  # Extract NSNP
+  nsnp <- strsplit(filename, "/")[[1]][5]
+
+  # Extract constrasts
+  tmp <- strsplit(filename, "/")[[1]][6]
+  tmp2 <- strsplit(tmp, "_")[[1]][3]
+  constrasts <- strsplit(tmp2, ".txt")[[1]][1]
+  method <- tmp2[[1]][2]
+
+  # Read in results
+  df <- fread(filename)
+  names_from_file <- colnames(df)
+  df$dataset <- dataset
+  df$gwas <- gwas
+  df$contrasts <- constrasts
+  df$nsnp <- nsnp
+  df$method <- method
+  colnames(dfOut) <- c(names_from_file, "dataset", "gwas", "contrasts", "nsnp", "method")
+  print(head(df))
+  dfOut <- rbind(dfOut, df)
+
+}
+
+# Remove first row
+dfOut <- as.data.frame(dfOut[2:nrow(dfOut),])
+
+# Save file
+print(dfOut)
+fwrite(dfOut,outfile, row.names = F, col.names = T, quote = F, sep = "\t")
+
+
+
+
