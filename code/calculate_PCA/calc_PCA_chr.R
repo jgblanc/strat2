94236852ae98c5dc6f298b5d47de11e52e37095b
jgblanc <jgblanc@uchicago.edu>
2025-04-13 17:38:41 -0500

    t

--- code/calculate_PCA/calc_PCA_chr.R
+++ code/calculate_PCA/calc_PCA_chr.R
@@ -2,7 +2,7 @@
 
 args=commandArgs(TRUE)
 
-if(length(args)<4){stop("Rscript calc_FGr.R <prefix to plink files> <r prefix> <var prefix> <out prefix> <snp> <ids> <outfile>")}
+if(length(args)<3){stop("Rscript calc_FGr.R <prefix to plink files> <r prefix> <var prefix> <out prefix> <snp> <ids> <outfile>")}
 
 suppressWarnings(suppressMessages({
   library(data.table)
@@ -20,8 +20,19 @@ dfIDs <- fread(id_file)
 M <- nrow(dfIDs)
 
 
+# Do chromsome 1 separately
+
+# Set up parameters
+chr_str <- paste0("1")
+plink_out <- paste0(plink_prefix, "_1")
+
+# Run plink
+plink_cmd <- paste0("plink2 --pfile ", plink_prefix, " --keep ", id_file, " --chr ", chr_str ," --threads 8 --pca 40 approx --memory 38000 ", "--out ", plink_out)
+system(plink_cmd)
+
+
 # Loop through Chromosome
-for (i in 1:22) {
+for (i in 2:22) {
 
   print(paste0("Running plinke for chromosomes 1 through ", i))
 
@@ -30,8 +41,7 @@ for (i in 1:22) {
   plink_out <- paste0(plink_prefix, "_", "i")
 
   # Run plink
-  plink_cmd <- paste0("plink2 --pfile ", plink_prefix, " --keep ", id_file, " --chr ", chr_str ," --threads 8 ----pca 40 approx
-                      --memory 38000 ", "--out ", plink_out)
+  plink_cmd <- paste0("plink2 --pfile ", plink_prefix, " --keep ", id_file, " --chr ", chr_str ," --threads 8 --pca 40 approx --memory 38000 ", "--out ", plink_out)
   system(plink_cmd)
 
 }
