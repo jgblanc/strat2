09df3969af11f3a44493a09fb3d10d9af028ab11
jgblanc <jgblanc@uchicago.edu>
2025-04-17 12:00:34 -0500

    t

---
+++ code/calculate_FGr/R2_EO.R
@@ -0,0 +1,137 @@
+## Compute FGr using one PC SNPs
+
+args=commandArgs(TRUE)
+
+if(length(args)<3){stop("Rscript calc_FGr.R <prefix to plink files> <r prefix> <var prefix> <out prefix> <snp> <ids> <outfile>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(tidyverse)
+}))
+
+pca_file = args[1]
+fgr_file = args[2]
+snp_file = args[3]
+out_file = args[4]
+
+
+# Read PCA
+PCs <- fread(pca_file)
+PC_IDs <- PCs[,1:2]
+PC_nums <- PCs[,3:ncol(PCs)]
+
+# Scale the PCs to have variance 1
+PC_nums <- scale(PC_nums)
+
+# Figure out number of FGr Chrs
+chrFGr <- seq(2, 22, 2)
+
+# Read in SNP num files
+dfSNP <- fread(snp_file)
+
+# Extract only block nums with correct chr
+blockIndex <- which(dfSNP$CHR %in% chrFGr)
+dfSNP_filter <- dfSNP %>% filter(CHR %in% chrFGr)
+L <- sum(dfSNP_filter$nSNP)
+numBlocks <- length(blockIndex)
+
+# Read in and compute FGr
+dfFGr <- as.matrix(fread(fgr_file))
+dfF_select <- dfFGr[,blockIndex]
+FGr_raw <- apply(dfF_select, 1, sum)
+FGr <- FGr_raw * (1/(sqrt(L-1)))
+M <- length(FGr)
+
+# Compute Jacknife of each FGR
+jckFGr <- matrix(NA, nrow = M, ncol = numBlocks)
+for (i in 1:numBlocks) {
+
+  print(paste0("This is rep number ",i))
+  mi <- as.numeric(dfSNP_filter[i,2])
+  FGri <- (FGr_raw - dfF_select[,i]) * (1/sqrt(L-mi-1))
+  jckFGr[,i] <- (FGr - FGri)^2  * ((L - mi)/mi)
+}
+
+# Compute Numerator for error
+meanJCK <- rowMeans(jckFGr)
+numerator <- mean(meanJCK)
+print(paste0("The numerator is ",numerator))
+
+# Compute Denominator
+varFGr <- var(FGr)
+
+# Find Error
+error <- numerator / varFGr
+
+# Final signal
+signal <- 1 - error
+
+
+# Find R2 for each PC
+
+# Compute R2 function
+compute_Ratio <- function(Fvec, PC, signal) {
+
+  mod <- lm(Fvec ~ PC)
+  R2 <- summary(mod)$r.squared
+  Ratio <- R2/signal
+
+  return(Ratio)
+}
+
+# Bootstrap function
+bootstrap_ratio_ci <- function(Fvec, PC, signal, n_boot = 1000, conf = 0.95) {
+
+  PC <- as.matrix(PC)
+  n <- length(Fvec)
+
+  boot_ratios <- replicate(n_boot, {
+    idx <- sample(n, replace = TRUE)
+    compute_Ratio(Fvec[idx], PC[idx, , drop = FALSE], signal)
+  })
+
+  alpha <- 1 - conf
+  ci <- quantile(boot_ratios, probs = c(alpha / 2, 1 - alpha / 2))
+  return(list(
+    estimate = compute_Ratio(Fvec, PC, signal),
+    se = sd(boot_ratios),
+    ci = ci
+  ))
+}
+
+# Construct output
+dfOut <- matrix(NA, nrow=ncol(PC_nums), ncol = 8)
+colnames(dfOut) <- c("PC", "B2", "R2", "Signal", "Ratio", "lc", "uc", "se")
+FGr_scale <- scale(FGr)
+
+
+# Loop through PCs
+for (i in 1:ncol(PC_nums)) {
+
+  print(i)
+  # Calc cov
+  B <- cov(FGr_scale, PC_nums[,i])
+  B2 <- B^2
+
+  # Collect output
+  dfOut[i, 1] <- i
+  dfOut[i,2] <- B2
+  R2 <- sum(dfOut[1:i, 2])
+  dfOut[i,3] <- R2
+  dfOut[i,4] <- signal
+  Ratio <- R2/signal
+  dfOut[i,5] <- Ratio
+
+  # Get CI
+  result <- bootstrap_ratio_ci(FGr_scale, PC_nums[,1:i], signal, n_boot = 100, conf = 0.95)
+  dfOut[i,8] <- result$se
+  dfOut[i,6] <- as.numeric(result$ci[1])
+  dfOut[i,7] <- as.numeric(result$ci[2])
+
+}
+
+# Save output file
+fwrite(dfOut, out_file, quote = F, row.names = F, sep = "\t")
+
+
+
