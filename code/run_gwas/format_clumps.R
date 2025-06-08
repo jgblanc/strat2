5339e3f6c582b2775d93f6942b09b6df5eb67a95
jgblanc <jgblanc@uchicago.edu>
2025-06-08 18:18:05 -0500

    t

---
+++ code/run_gwas/format_clumps.R
@@ -0,0 +1,27 @@
+## Format phenotype file for REGENIE
+
+args=commandArgs(TRUE)
+
+if(length(args)<2){stop("Rscript calc_R2_EO.R")}
+
+
+library(data.table)
+library(tidyverse)
+
+ss_file = args[1]
+clump_file = args[2]
+out_file = args[3]
+
+# Read in SS
+dfSNPs <- fread(snp_file)
+
+# Read clumps
+dfClump <- fread(clump_file)
+
+# Join files
+dfOut <- inner_join(dfSNPs, dfClump)
+dfOut <- dfOut %>% select("#CHROM", "ID", "ALLELE0", "ALLELE1", "BETA")
+colnames(dfOut) <- c("#CHROM", "ID", "REF", "ALT", "BETA")
+
+# Write output
+fwrite(dfOut, out_file,row.names = FALSE, sep = "\t", quote = FALSE,na = "NA")
