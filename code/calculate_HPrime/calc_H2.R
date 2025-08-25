4cb91bb5bbe779ec93484d58167dbe3d28d664e7
jgblanc <jgblanc@uchicago.edu>
2025-08-25 09:24:28 -0500

    t

---
+++ code/calculate_HPrime/calc_H2.R
@@ -0,0 +1,183 @@
+
+## Compute H using one SNP per independent block
+
+args=commandArgs(TRUE)
+
+if(length(args)<5){stop("Rscript calc_H.R <prefix to plink files> <r prefix> <var prefix> <out prefix> <snp> <ids> <outfile>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(tidyverse)
+}))
+
+resid_prefix = args[1]
+r_prefix = args[2]
+snp_file = args[3]
+id_file = args[4]
+out_file = args[5]
+
+
+# Read in IDs
+dfIDs <- fread(id_file)
+
+# Set data collectors
+M <- nrow(dfIDs)
+
+# Read in all values of r
+
+## First Chr
+r_file_name <- paste0(r_prefix, 1, ".rvec")
+df <- fread(r_file_name)
+df$CHR <- 1
+
+## All other Chrs
+for (i in 2:22) {
+
+  # Read in R file
+  r_file_name <- paste0(r_prefix, i, ".rvec")
+  tmp <- fread(r_file_name)
+  tmp$CHR <- i
+
+  # Combine
+  df <- rbind(df, tmp)
+}
+print(paste0("There are ", nrow(df), " SNPs in all the R files"))
+
+# Read in SNP file
+dfSNP <- fread(snp_file) %>% select("ID", "block")
+print(paste0("Number of PC SNPs ", nrow(dfSNP)))
+
+# Combine SNP and R files
+dfALL <- inner_join(df, dfSNP) %>% drop_na()
+print(paste0("There are ", nrow(dfALL), " SNPs in all the R files combined with the pruned SNPs"))
+L <- nrow(dfALL)
+print(L)
+
+# Standardize r values variance
+dfALL$r <- dfALL$r / sd(dfALL$r)
+print(paste0("The variance of r is ", var(dfALL$r)))
+print(paste0("The mean of r is ", mean(dfALL$r)))
+
+# Set up data frame to collect SNP numbers
+numBlocks <- length(unique(dfALL$block))
+dfSNPs <- as.data.frame(matrix(NA, ncol = 2, nrow = numBlocks))
+colnames(dfSNPs) <- c("Block", "nSNP")
+print(paste0("The total number of blocks is ", numBlocks))
+dfFGr_mat <- matrix(NA, nrow = M, ncol = numBlocks)
+
+
+
+counter <- 1
+for (i in 1:22) {
+
+  # Get r values on Chr i
+  dfR_chr <- dfALL %>% filter(CHR == i)
+  chr_blocks <- unique(dfR_chr$block)
+  num_blocks_chr <- length(chr_blocks)
+  print(paste0("The number of blocks on chr ", i, " is ", num_blocks_chr))
+
+  # Get SNP ids on Chr i
+  traw_file <- paste0(resid_prefix, i, "_residual.traw ")
+  cmd_file<- paste0( "cut -f1 ",traw_file)
+  dfSNPchr <- fread(cmd = cmd_file,header = TRUE)
+
+  for (b in 44:44) {
+
+    print(paste0("We are on block", b))
+    # Select r values on block b
+    dfR_block <- dfR_chr %>% filter(block == b)
+    print(nrow(dfR_block))
+    print(head(dfR_block))
+    print(tail(dfR_block))
+    r_ids <- dfR_block$ID
+    print(length(r_ids))
+
+    # Get the right row values
+    row_ids <- which(dfSNPchr$SNP %in% r_ids)
+    print(paste0("There are ", length(row_ids), " SNPs on the block" ))
+
+    # read only correct row IDs
+    row_nums_str <- paste(row_ids + 1, collapse = ",")  # +1 for header row
+    cmd_block <- paste0("awk 'NR==1 || NR==", gsub(",", " || NR==", row_nums_str), "' ", traw_file)
+    tmpfile <- paste0(out_file, "_tmp.txt")
+    system(paste(cmd_block, ">", tmpfile))
+    k <- floor(length(row_ids)/3)
+    print(k)
+    dfBlock1 <- fread(tmpfile,nrow=k, header=TRUE)
+    dfBlock2 <- fread(tmpfile, skip=k+1, nrow=k, header=FALSE)
+    dfBlock3 <- fread(tmpfile, skip=(2*k)+1, header=FALSE)
+    setnames(dfBlock2, names(dfBlock1))
+    setnames(dfBlock3, names(dfBlock1))
+    dfBlock <- rbind(dfBlock1, dfBlock2)
+    dfBlock <- rbind(dfBlock, dfBlock3)
+    print(nrow(dfBlock))
+    system(paste("rm", tmpfile))
+    
+
+    # read only correct row IDs
+    #row_nums_str <- paste(row_ids + 1, collapse = ",")  # +1 for header row
+    #cmd_block <- paste0("awk 'NR==1 || NR==", gsub(",", " || NR==", row_nums_str), "' ", traw_file)
+    #tmpfile <-  "test.txt"
+    #system(paste(cmd_block, ">", tmpfile))
+    #dfBlock <- fread(tmpfile, header = TRUE)
+    ##dfBlock <- fread(cmd = cmd_block, header=TRUE)
+    #print(nrow(dfBlock))
+    #print(dfBlock[1,1:10])
+
+    # do matrix multiplication
+    matBlock <- t(as.matrix(dfBlock[,4:ncol(dfBlock)]))
+    matBlock <- scale(matBlock)
+    x <- dfR_blocks %>%	filter(r_ids %in% dfR_blocks$ID)
+    print(paste0("The dim of matBlock is ", dim(matBlock)))
+    print(paste0("The dim of matBlock is ", dim(as.matrix(x))))
+    matf <- matBlock %*% x
+    matf_raw <- apply(matf, 1, sum)
+    dfFGr_mat[,counter] <- matf_raw
+
+    # Save number of SNPs
+    nsnp_in_block <- nrow(dfR_block)
+    dfSNPs[counter,1] <- b
+    dfSNPs[counter,2] <- nrow(dfR_block)
+    counter <- counter + 1
+
+  }
+}
+
+# Calculate FGr
+FGr_raw <- apply(dfFGr_mat, 1, sum)
+print(paste0("The raw var is ", var(FGr_raw)))
+
+# Scale by 1/sqrt(L-1)
+FGr <- FGr_raw * (1/(sqrt(L-1)))
+print(paste0("The scaled var is ", var(FGr)))
+
+# Calculate H
+H <- (1/(M * (L-1))) * (t(FGr) %*% FGr)
+print(paste0("H is ", H))
+print(paste0("1/L is ", 1/L))
+
+# Compute SE for H
+allHs <- rep(NA, numBlocks)
+allHi <- rep(NA, numBlocks)
+for (i in 1:numBlocks) {
+
+  print(paste0("This is rep number ",i))
+  mi <- as.numeric(dfSNPs[i,2])
+  FGri <- (FGr_raw - dfFGr_mat[,i]) * (1/sqrt(L-mi-1))
+  Hi <- sum(FGri^2) * (1/M) * (1/(L-mi-1))
+  allHs[i] <- ((L - mi)/mi) * (H - Hi)^2
+  allHi[i] <- Hi
+
+}
+varH <- mean(allHs)
+meanH <- mean(allHi)
+
+# P-value from sims
+pvalNorm <- pnorm(H, mean = 1/L, sd=sqrt(varH), lower.tail = FALSE)
+
+# Construct output
+dfOut <- data.frame(H = H, L = L, meanH = meanH, varH = varH, pvalNorm = pvalNorm)
+fwrite(dfOut, out_file, quote = F, row.names = F, sep = "\t")
+
+
+
