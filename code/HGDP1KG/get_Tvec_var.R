bd6ab429cb86963cc6b9e4ed1914e697d9056a44
Jennifer Blanc <jgblanc@Jennifers-MacBook-Air.local>
2025-08-26 14:51:06 -0500

    t

---
+++ code/HGDP1KG/get_Tvec_var.R
@@ -0,0 +1,28 @@
+## Get Lat/Long Test Vector
+## This script uses HGDP metadata and a psam to format test vectors for latitide and longitude
+
+args=commandArgs(TRUE)
+
+if(length(args)<4){stop("Rscript get_LatLong_Tvec.R <psam> <outfile Lat> <outfile Long>")}
+
+suppressWarnings(suppressMessages({
+  library(data.table)
+  library(dplyr)
+}))
+
+infile_lat= args[1]
+outfile_lat = args[2]
+infile_long = args[3]
+outfile_long = args[4]
+
+# Read in lat
+dfLat <- fread(infile_lat)
+varTvec_lat <- var(dfLat$lat)
+
+# Read in lat
+dfLong <- fread(infile_long)
+varTvec_long <- var(dfLong$long)
+
+# Save output
+fwrite(as.data.frame(varTvec = varTvec_lat),outfile_lat, row.names = F, col.names = T, quote = F, sep = "\t")
+fwrite(as.data.frame(varTvec = varTvec_long),outfile_long, row.names = F, col.names = T, quote = F, sep = "\t")
