dc99b0c2dc412b86d7a99c184d84a01c160e8d20
jgblanc <jgblanc@uchicago.edu>
2025-08-28 11:29:07 -0500

    t

--- code/HGDP1KG/get_Tvec_all.R
+++ code/HGDP1KG/get_Tvec_all.R
@@ -22,7 +22,7 @@ df <- dfIDs %>% filter(pop %in% c("eas", "nfe")) %>% mutate(tvec = case_when(pop
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[1] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[1] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 
 # EAS-SAS
@@ -30,21 +30,21 @@ df <- dfIDs %>% filter(pop %in% c("eas", "sas"))%>% mutate(tvec = case_when(pop 
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[2] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[2] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # EAS-AFR
 df <- dfIDs %>% filter(pop %in% c("eas", "afr"))%>% mutate(tvec = case_when(pop == "eas" ~ 1, pop == "afr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[3] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[3] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # EAS-AMR
 df <- dfIDs %>% filter(pop %in% c("eas", "amr"))%>% mutate(tvec = case_when(pop == "eas" ~ 1, pop == "amr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[4] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[4] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 
 # NFE-SAS
@@ -52,42 +52,42 @@ df <- dfIDs %>% filter(pop %in% c("nfe", "sas")) %>% mutate(tvec = case_when(pop
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[5] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[5] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # NFE-AFR
 df <- dfIDs %>% filter(pop %in% c("nfe", "afr")) %>% mutate(tvec = case_when(pop == "nfe" ~ 1, pop == "afr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[6] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[6] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # NFE-AMR
 df <- dfIDs %>% filter(pop %in% c("nfe", "amr")) %>% mutate(tvec = case_when(pop == "nfe" ~ 1, pop == "amr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[7] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[7] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # SAS-AFR
 df <- dfIDs %>% filter(pop %in% c("sas", "afr")) %>% mutate(tvec = case_when(pop == "sas" ~ 1, pop == "afr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[8] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[8] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # SAS-AMR
 df <- dfIDs %>% filter(pop %in% c("sas", "amr")) %>% mutate(tvec = case_when(pop == "sas" ~ 1, pop == "amr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[9] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[9] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 # AFR-AMR
 df <- dfIDs %>%  filter(pop %in% c("afr", "amr")) %>% mutate(tvec = case_when(pop == "afr" ~ 1, pop == "amr" ~ -1))
 df$tvec<- scale(df$tvec)
 print(paste0("The mean of Tvec is ", mean(df$tvec)))
 print(paste0("The variance of Tvec is ", var(df$tvec)))
-fwrite(dfOut, args[10] ,row.names=F,quote=F,sep="\t", col.names = T)
+fwrite(df, args[10] ,row.names=F,quote=F,sep="\t", col.names = T)
 
 
 
