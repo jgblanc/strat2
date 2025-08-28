cc47ebfb06f7b6a8d8c636e888b3f790b31af399
Jennifer Blanc <jgblanc@Jennifers-MacBook-Air.local>
2025-08-28 10:46:06 -0500

    t

--- code/HGDP1KG/compute_XT.sh
+++ code/HGDP1KG/compute_XT.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 pfile_path=$1
 pheno_path=$2
-test_type=$3
-outfile=$4
-overlap_snps=$5
+outfile=$3
+overlap_snps=$4
 
 plink2 \
   --pfile $pfile_path \
   --extract $overlap_snps \
   --glm omit-ref allow-no-covars \
   --pheno $pheno_path \
-  --pheno-name $test_type \
-  --geno-counts \
+  --pheno-name tvec \
   --out $outfile
