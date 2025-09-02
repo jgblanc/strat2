98022a420b53804e1bd8730278f46d3c92de0284
jgblanc <jgblanc@uchicago.edu>
2025-09-02 12:20:23 -0500

    t

--- code/HGDP1KG/compute_XT.sh
+++ code/HGDP1KG/compute_XT.sh
@@ -8,6 +8,7 @@ plink2 \
   --pfile $pfile_path \
   --extract $overlap_snps \
   --glm omit-ref allow-no-covars \
+  --no-input-missing-phenotype \
   --pheno $pheno_path \
   --pheno-name tvec \
   --out $outfile
