ae08c215e6ea8c32a03bdd2294e0855c020bd966
Jennifer Blanc <jgblanc@Jennifers-MacBook-Air.local>
2025-09-09 09:37:43 -0500

    t

--- code/calculate_FGr/R2_EO.R
+++ code/calculate_FGr/R2_EO.R
@@ -17,6 +17,47 @@ pca_file = args[1]
 fgr_file = args[2]
 snp_file = args[3]
 out_file = args[4]
+r_prefix = args[5]
+
+####################################
+########## Functions ###############
+####################################
+
+
+calc_fhat <- function(dfMat, r ) {
+
+  r <- r - mean(r)
+  fhat_raw <- apply(dfMat, 1, sum)
+  rTr <- as.numeric(t(as.matrix(r)) %*% as.matrix(r))
+  fhat <- fhat_raw / c(rTr)
+
+  return(fhat)
+}
+
+calc_sigma2_f <- function(fhat, M) {
+
+  fhat <- fhat - mean(fhat)
+  numerator <- as.numeric(t(fhat) %*% fhat)
+  out <- numerator / (M-1)
+
+  return(out)
+}
+
+
+calc_sigma2_r <-function(r, L) {
+
+  r <- r - mean(r)
+  rTr <- as.numeric(t(as.matrix(r)) %*% as.matrix(r))
+  out <- rTr / (L - 1)
+
+  return(out)
+}
+
+
+####################################
+########## Main  ###################
+####################################
+
 
 # Read inputs
 PCs <- fread(pca_file)
@@ -27,105 +68,107 @@ dfSNP <- fread(snp_file)
 chrFGr <- seq(2, 22, 2)
 dfSNP_filter <- dfSNP %>% filter(CHR %in% chrFGr)
 blockIndex <- which(dfSNP$CHR %in% chrFGr)
-L <- sum(dfSNP_filter$nSNP)
 numBlocks <- length(blockIndex)
+print(paste0("There are ", numBlocks, "on even Chromosomes"))
+
+# Read in R values
+
+## First Chr
+r_file_name <- paste0(r_prefix, 2, ".rvec")
+df <- fread(r_file_name)
+df$CHR <- 2
+
+## All other Chrs
+for (i in seq(4, 22, 2)) {
+
+  # Read in R file
+  r_file_name <- paste0(r_prefix, i, ".rvec")
+  tmp <- fread(r_file_name)
+  tmp$CHR <- i
+
+  # Combine
+  df <- rbind(df, tmp)
+}
+print(paste0("There are ", nrow(df), " SNPs in all the R files"))
+
+# Read in SNP file
+dfSNP <- fread(pc_snp_file)  %>% select("ID", "block")
+print(paste0("Number of PC SNPs ", nrow(dfSNP)))
+
+# Combine SNP and R files
+dfALL <- inner_join(df, dfSNP) %>% drop_na() %>% filter(CHR %in% chrFGr)
+print(paste0("There are ", nrow(dfALL), " SNPs in all the R files combined with the pruned SNPs"))
+L <- nrow(dfALL)
+print(L)
 
 # Read in and compute FGr
-dfFGr <- as.matrix(fread(fgr_file))[, blockIndex]
-FGr_raw <- rowSums(dfFGr)
-FGr <- FGr_raw / sqrt(L - 1)
-M <- length(FGr)
+dfMat <- as.matrix(fread(fgr_file))[, blockIndex]
+fhat <- calc_fhat(dfMat, dfALL$r)
+M <- length(fhat)
 
-# Calculate H
-H <- (1 / (M * (L - 1))) * sum(FGr^2)
-print(H)
+### Calculate H Real
+sigma2F <- as.numeric(calc_sigma2_f(fhat, M))
+print(paste0("Sigma2F is ", sigma2F))
+sigma2r <- as.numeric(calc_sigma2_r(dfALL$r, L))
+print(paste0("Sigma2r is ", sigma2r))
+H <- as.numeric(sigma2F * sigma2r)
+print(paste0("H is ", H))
 
-# Compute jackknife estimates
-mi_vec <- dfSNP_filter$nSNP
-FGri_mat <- FGr_raw - dfFGr
-scale_factors <- 1 / sqrt(L - mi_vec - 1)
-FGri_scaled <- sweep(FGri_mat, 2, scale_factors, `*`)
-Hi_vec <- colSums(FGri_scaled^2) / (L - mi_vec - 1) / M
-allHs <- ((L - mi_vec) / mi_vec) * (H - Hi_vec)^2
-varH <- mean(allHs)
+### Calculate signal
 
-# Jackknife variance of each FGr element
-FGr_mat <- matrix(rep(FGr, numBlocks), nrow = M)
-FGri_mat2 <- FGri_scaled
-jckFGr <- ((FGr_mat - FGri_mat2)^2) * matrix((L - mi_vec) / mi_vec, M, numBlocks, byrow = TRUE)
-meanJCK <- rowMeans(jckFGr)
-numerator <- mean(meanJCK)
-varFGr <- var(FGr)
-error <- numerator / varFGr
-signal <- 1 - error
+# Leave one out f;s
+jckFGr <- matrix(NA, nrow = M, ncol = numBlocks)
+for (i in 1:numBlocks) {
 
+  # Block num
+  blockNum <- as.numeric(dfSNP_filter[i,1])
+  print(paste0("This is rep number ",i))
+  print(paste0("This is block number ",blockNum))
+  dfR_not_i <- dfALL %>% filter(block != blockNum)
+  dfR_i <- dfALL %>% filter(block == blockNum)
+  mi <- nrow(dfR_i)
+  fhat_i <- calc_fhat(dfMat[,-i],dfR_not_i$r)
+  jckFGr[,i] <- ((L - mi)/mi) * (fhat - fhat_i)^2
 
-# Bootstrap helper
-compute_Ratio <- function(Fmat, PC) {
-
-  FGr_raw <- rowSums(Fmat)
-  FGr <- FGr_raw / sqrt(L - 1)
-  mod <- lm(scale(FGr) ~ PC)
-  R2 <- summary(mod)$r.squared
-  f <- summary(mod)$fstatistic
-  p <- pf(f[1],f[2],f[3],lower.tail=F)
-
-  # Jackknife error for this Fmat
-  FGri_mat <- FGr_raw - Fmat
-  FGri_scaled <- sweep(FGri_mat, 2, scale_factors, `*`)
-  jckFGr <- ((FGr - FGri_scaled)^2) * matrix((L - mi_vec) / mi_vec, nrow(Fmat), numBlocks, byrow = TRUE)
-  numerator <- mean(rowMeans(jckFGr))
-  signal <- 1 - (numerator / var(FGr))
-
-  # Ratio
-  ratio <- R2/signal
-
-  return(list(R2 = R2, p=p, Ratio = ratio))
 }
 
+# Compute numerator
+print(jckFGr)
+tmp <- apply(jckFGr, 1, mean)
+print(jckFGr)
+numerator <- mean(tmp)
+print(paste0("The numerator is ",numerator))
+
+
+# Compute Denominator
+varFGr <- var(fhat)
+print(paste0("The denominator is ",varFGr))
+
+# Find Error
+error <- numerator / varFGr
+
+# Final signal
+signal <- 1 - error
+
 # Construct output
-dfOut <- matrix(NA, nrow = ncol(PC_nums), ncol = 16)
-colnames(dfOut) <- c("PC","H", "varH","signal", "B","pvalB", "lcB", "ucB", "B2", "R2","pvalR2", "lcR2", "ucR2", "Ratio", "lcRatio", "ucRatio")
+dfOut <- matrix(NA, nrow = ncol(PC_nums), ncol = 5)
+colnames(dfOut) <- c("PC","H","signal", "omega", "Ratio")
 
 # Loop through PCs
 for (i in seq_len(ncol(PC_nums))) {
 
   # Get Single PC stats
-  mod <- lm(scale(FGr)~ PC_nums[,i])
-  B <- mod$coefficients[2]
-  lcB <- confint(mod)[-1, ][1]
-  ucB <- confint(mod)[-1, ][2]
-  pvalB <- summary(mod)$coefficients[2,4]
-  B2 <- B^2
+  mod <- lm(fhat ~ PC_nums[,i])
+  w <- mod$coefficients[2]
 
-  # Get Combined R^2 stats
-  tmp <- compute_Ratio(Fmat = dfFGr, PC = PC_nums[,1:i])
-  pvalR2 <- tmp$p
-  R2 <- tmp$R2
-
-  # Run bootstrappin
-  #boot_ratios <- future_replicate(100, {
-  #  idx <- sample(M, replace = TRUE)
-  #  compute_Ratio(dfFGr[idx,], tmpPC <- PC_nums[idx,1:i])
-  #})
-  #boot_ratios_matrix <- matrix(unlist(boot_ratios), ncol = 2, byrow = TRUE)
-
-  # Get CI's for R2
-  #boot_R2_values <- boot_ratios_matrix[,1]
-  #ci <- quantile(boot_R2_values, probs = c(0.025, 0.975))
-  #lcR2 <- ci[1]
-  #ucR2 <- ci[2]
+  # Fit all PCs
+  mod  <- lm(fhat ~ PC_nums[,1:i])
+  R2 <- summary(mod)$r.squared
 
   # Get Ratio
-  Ratio <- tmp$Ratio
+  Ratio <- R2 / signal
 
-  # Get CI's for Ratio
-  #boot_ratio_values <- boot_ratios_matrix[,2]
-  #ci <- quantile(boot_ratio_values, probs = c(0.025, 0.975))
-  #lcRatio <- ci[1]
-  #ucRatio <- ci[2]
-
-  dfOut[i,] <- c(i, H, varH, signal, B,pvalB, lcB, ucB, B2, R2, pvalR2, NA, NA, Ratio, NA, NA)
+  dfOut[i,] <- c(i, H, signal, w, R2, Ratio)
   cat("Finished PC", i, "\n")
 }
 
